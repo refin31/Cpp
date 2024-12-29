44cde7f64e05cc12dafc6e432bd0432de922c214
Iqbal Refin Ardhani <138165583+refin31@users.noreply.github.com>
2024-12-29 20:30:53 +0700

    Add files via upload

---
+++ Basics/assignment.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	// Assignment
+	int num = 10;
+	
+	// num = num + 5;
+	num += 5; // 15
+	cout << num << "\n";
+	
+	// num = num - 5;
+	num -= 5; // 10
+	cout << num << "\n";
+	
+	// num = num * 2;
+	num *= 2; // 20
+	cout << num << "\n";
+	
+	// num = num / 4;
+	num /= 4; // 5
+	cout << num << "\n";
+	
+	// num = num % 3;
+	num %= 3; // 2
+	cout << num << "\n";
+	
+	return 0;
+}
