cf68bb62c0f4eb129947b4d6fd938f0d6798ab5a
Iqbal Refin Ardhani <138165583+refin31@users.noreply.github.com>
2024-12-29 07:34:04 +0700

    Add files via upload

---
+++ Basics/deklarasi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a;
+	a = 10;
+	cout << a << endl;
+	
+	int b;
+	cout << "Masukan nilai: ";
+	cin >> b;
+	cout << "Nilai yang anda masukan adalah: ";
+	cout << b;
+	return 0;
+}
