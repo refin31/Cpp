6df6bacaa1e1a60b2c660e0a09418639fd3b5a75
Iqbal Refin Ardhani <138165583+refin31@users.noreply.github.com>
2025-01-01 08:34:35 +0700

    Add files via upload

---
+++ Basics/while_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int countdown = 10;
+	
+	while(countdown > 0) {
+		cout << countdown << endl;
+		countdown--;
+	}
+	
+	cout << "Happy New Year!!";
+	
+	return 0;
+}
