ea8cead816e511a6f050f8bd8ec9f62907c7a0db
Iqbal Refin Ardhani <138165583+refin31@users.noreply.github.com>
2025-01-02 14:52:24 +0700

    Add files via upload

---
+++ Basics/fungsi.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+/*
+library cmath : referensi www.cppreference.com
+ceil(x)    	<- pembulatan ke atas
+cos(x) 		<- cosinus
+exp(x) 		<- eksponen
+fabs(x)    	<- nilai absolut dalam float
+floor(x)   	<- pembulatan ke bawah
+fmod(x)    	<- modulus dalam float
+log(x) 		<- logaritma dengan basis natural
+log10(x).  	<- logaritma dengan basis 10
+pow(x,y).  	<- x pangkat y
+sin(x) 		<- sinus
+sqrt(x).   	<- akar kuadrat
+tan(x) 		<- tangen
+*/
+
+
+using namespace std;
+
+int main() {
+	
+	int x;
+	
+	cout << "Masukan nilai x: ";
+	cin >> x;
+	
+	double y = sqrt(x);
+	cout << "Akarnya adalah: " << y << endl;
+	
+	return 0;
+}
