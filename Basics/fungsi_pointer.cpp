79ce1eed7afddd1db6b87677a549b3712f4bdd66
Iqbal Refin Ardhani <138165583+refin31@users.noreply.github.com>
2025-01-06 15:08:31 +0700

    Add files via upload

---
+++ Basics/fungsi_pointer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void fungsi(int *);
+void kuadrat(int *);
+
+int main() {
+ 
+ int a = 5;
+ 
+ cout << "Nilai a = " << a << endl;
+ cout << "Address a = " << &a << endl << endl;
+ 
+ fungsi(&a); // fungsi dengan input pointer
+ kuadrat(&a);
+ 
+ cout << "Nilai a = " << a << endl;
+ 
+ return 0;
+}
+
+void fungsi(int *b) {
+ cout << "Nilai b = " << *b << endl << endl;
+ cout << "Address b = " << b << endl << endl;
+}
+
+void kuadrat(int *valPtr) {
+ *valPtr = *valPtr * *valPtr;
+}
